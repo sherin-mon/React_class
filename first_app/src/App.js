5cd33d558053af97c10ae461ead0e7a656e645ef
sherin <sherinbiju340@gmail.com>
2024-11-23 00:44:53 +0530

    Function Component using simple method

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -1,22 +1,13 @@
 
 import React from "react";
+import Fun from "./Components/Comp/Fun";
 
 function App() {
 
-  var A="sherin"
-  var a=90
-  var b=89
-  function aa(){
-    return(
-      <p>Welcome every one </p>
-    )
-  }
+ 
   return (
-    <>
-    <p>HI My name {A}</p>
-    <p>Sum of   {a+b} </p>
-    {aa()}
-    </>
+    
+    <Fun />
   );
 }
 
---
+++ first_app/src/Components/Comp/Fun.js
@@ -0,0 +1,7 @@
+import React from "react";
+function Fun(){
+    return(
+        <h2>Function Component</h2>
+    )
+}
+export default Fun;
