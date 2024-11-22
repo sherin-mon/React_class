1e1209608cddf05d44b31af00f3ac8ac1ad71a4a
sherin <sherinbiju340@gmail.com>
2024-11-23 00:12:04 +0530

    first commit

---
+++ first_app/src/App.js
@@ -0,0 +1,13 @@
+import Register from "./Register"
+
+function App() {
+  return (
+    <div>
+      <p> thus data is comming</p>
+      <Register />
+      </div>
+    
+  );
+}
+
+export default App;
---
+++ first_app/src/Register.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Register(){
+    return(
+        <h2>Data comming</h2>
+    )
+
+}
+export default Register;
